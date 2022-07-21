2f00ae712e90b01286b09467ec82b78495dd7e67
baichensong <1350356294@qq.com>
2022-07-21 18:44:48 +0800

    首次提交

---
+++ src/test/java/com/asong/study_total/StudyTotalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asong.study_total;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTotalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
